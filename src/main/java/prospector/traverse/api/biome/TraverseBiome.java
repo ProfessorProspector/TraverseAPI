1eac315ff5f4486ef26bc2effc10722dbfe06640
ProfessorProspector <thedoctorsoda@gmail.com>
2018-12-09 10:29:00 -0800

    Update stuff

--- src/main/java/prospector/traverse/api/biome/TraverseBiome.java
+++ src/main/java/prospector/traverse/api/biome/TraverseBiome.java
@@ -18,7 +18,7 @@ public class TraverseBiome extends Biome {
 	public final EnumMap<EntityCategory, List<Biome.SpawnEntry>> entitySpawns;
 
 	public TraverseBiome(BiomeInfo biomeInfo) {
-		super(new Builder().category(biomeInfo.getCategory()).parent(biomeInfo.getParent()).depth(biomeInfo.getDepth()).scale(biomeInfo.getScale()).temperature(biomeInfo.getTemperature()).downfall(biomeInfo.getDownfall()).precipitation(biomeInfo.getPrecipitation()).waterColor(biomeInfo.getWaterColor()).waterFogColor(biomeInfo.getWaterFogColor()).configureSurfaceBuilder(biomeInfo.getSurface().getSurfaceBuilder(), biomeInfo.getSurface().getConfig()));
+		super(new Settings().category(biomeInfo.getCategory()).parent(biomeInfo.getParent()).depth(biomeInfo.getDepth()).scale(biomeInfo.getScale()).temperature(biomeInfo.getTemperature()).downfall(biomeInfo.getDownfall()).precipitation(biomeInfo.getPrecipitation()).waterColor(biomeInfo.getWaterColor()).waterFogColor(biomeInfo.getWaterFogColor()).configureSurfaceBuilder(biomeInfo.getSurface().getSurfaceBuilder(), biomeInfo.getSurface().getConfig()));
 		this.grassColor = biomeInfo.getGrassColor() == null ? OptionalInt.empty() : OptionalInt.of(biomeInfo.getGrassColor());
 		this.foliageColor = biomeInfo.getFoliageColor() == null ? OptionalInt.empty() : OptionalInt.of(biomeInfo.getFoliageColor());
 		this.features = biomeInfo.getFeatures();
